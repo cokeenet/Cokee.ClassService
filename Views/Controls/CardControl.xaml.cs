4bede145f737d85f6833e112667d00e6c2d7f08e
Cokee <xiaoyangjunroms@outlook.com>
2024-05-04 17:45:38 +0800

    Update to iNKORE.UI

---
+++ Views/Controls/CardControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Cokee.ClassService.Views.Controls;
+
+public partial class CardControl : UserControl
+{
+    public CardControl()
+    {
+        InitializeComponent();
+    }
+}
