3679ed16e6c5e82b5e3f368af24b328e82a949a9
Cokee <xiaoyangjunroms@outlook.com>
2024-05-26 17:55:50 +0800

    1

--- Views/Controls/UsbCard.xaml.cs
+++ Views/Controls/UsbCard.xaml.cs
@@ -22,8 +22,6 @@ namespace Cokee.ClassService.Views.Controls
     public partial class UsbCard : UserControl
     {
         public string disk;
-        private BackgroundWorker picBackgroundWorker = new BackgroundWorker();
-        private Stopwatch sw = new Stopwatch();
 
         public UsbCard()
         {
@@ -31,11 +29,6 @@ namespace Cokee.ClassService.Views.Controls
             try
             {
                 if (DesignerProperties.GetIsInDesignMode(this)) return;
-                picBackgroundWorker.WorkerReportsProgress = true;
-                picBackgroundWorker.WorkerSupportsCancellation = true;
-                picBackgroundWorker.DoWork += BackgroundWorker1_DoWork;
-                picBackgroundWorker.ProgressChanged += BackgroundWorker1_ProgressChanged;
-                picBackgroundWorker.RunWorkerCompleted += BackgroundWorker1_RunWorkerCompleted;
                 if (Catalog.IsScrSave) return;
                 EnumDrive();
             }
@@ -176,9 +169,6 @@ namespace Cokee.ClassService.Views.Controls
             IntPtr lpOverlapped
         );
 
-        private string copyDisk;
-        private int copieditems = 0, copieddirs = 0;
-
         private void SymbolIcon_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             if (File.Exists(disk + "picDisk"))
@@ -196,77 +186,6 @@ namespace Cokee.ClassService.Views.Controls
                 }
             }
             else Catalog.ShowInfo("nonTag");
-        }
-
-        private void BackgroundWorker1_ProgressChanged(object? sender, ProgressChangedEventArgs e)
-        {
-            Catalog.UpdateProgress(e.ProgressPercentage, true, $"log{e.UserState}");
-        }
-
-        private void BackgroundWorker1_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
-        {
-            sw.Stop();
-            if (e.Error != null)
-                Catalog.ShowInfo($"Debug日志 threw Exception. ({sw.Elapsed.Seconds}s)", $"Exception:{e.Error.ToString()}");
-            else if (e.Cancelled)
-                Catalog.ShowInfo($"Debug日志 Cancelled:{e.Cancelled} ({sw.Elapsed.Seconds}s)", $"Exception:{e.Error?.ToString()}");
-            else
-                Catalog.ShowInfo($"Debug日志 Completed. ({sw.Elapsed.Seconds}s)", $"Result:{e.Result?.ToString()}");
-            Catalog.UpdateProgress(100, false);
-        }
-
-        private void BackgroundWorker1_DoWork(object? sender, DoWorkEventArgs e)
-        {
-            Log.Information("正在生成程序Debug日志");
-            copieddirs = 0;
-            copieditems = 0;
-            foreach (string dir in Directory.GetDirectories("D:\\CokeeDP\\Cache"))
-            {
-                DirectoryInfo dirinfo = new DirectoryInfo(dir);
-                var files = Directory.GetFiles(dir);
-                var dirs = Directory.GetDirectories(dir);
-                if (dirs != null)
-                {
-                    if (dirs.Length > 0)
-                    {
-                        foreach (string subdir in dirs)
-                        {
-                            var subinfo = new DirectoryInfo(subdir);
-                            decimal num = 0;
-                            var cpSubTo = $"{copyDisk}\\CokeeDP\\Cache\\{dirinfo.Name}\\{subinfo.Name}";
-                            DirHelper.MakeExist(cpSubTo);
-                            var subfiles = Directory.GetFiles(subdir);
-                            Log.Information($"Found v2 pic dir {subinfo.Name} with {subfiles.Length} pics.");
-                            foreach (var item in subfiles)
-                            {
-                                FileInfo f = new FileInfo(item);
-                                if (!File.Exists($"{cpSubTo}\\{f.Name}"))
-                                    f.CopyTo($"{cpSubTo}\\{f.Name}");
-                                copieditems++;
-                                num++;
-                                picBackgroundWorker.ReportProgress(Convert.ToInt32(num / (decimal)subfiles.Length * 100), "v2" + subinfo.Name);
-                            }
-                            copieddirs++;
-                            Log.Information("Done.");
-                        }
-                    }
-                }
-                var cpTo = copyDisk + $"CokeeDP\\Cache\\{dirinfo.Name}";
-                DirHelper.MakeExist(cpTo);
-                decimal num1 = 0;
-                Log.Information($"Found v1 pic dir {dirinfo.Name} with {files.Length} pics.");
-                foreach (string file in files)
-                {
-                    FileInfo f = new FileInfo(file);
-                    if (!File.Exists($"{cpTo}\\{f.Name}"))
-                        f.CopyTo($"{cpTo}\\{f.Name}");
-                    num1++;
-                    // Log.Information($"{dirinfo.Name}:{num}/{files.Length}");
-                    picBackgroundWorker.ReportProgress(Convert.ToInt32(num1 / (decimal)files.Length * 100), dirinfo.Name);
-                }
-                Log.Information("Done.");
-                e.Result = $"{copieddirs} dirs,{copieditems} items";
-            }
         }
     }
 }
---
+++ Views/Pages/Debug.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Cokee.ClassService.Helper;
+
+namespace Cokee.ClassService.Views.Pages
+{
+    /// <summary>
+    /// Debug.xaml 的交互逻辑
+    /// </summary>
+    public partial class Debug : Page
+    {
+        public Debug()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            switch (btn.Tag.ToString())
+            {
+                case "1":
+                    List<string> dirs = new List<string>();
+                    foreach (string str in Directory.GetDirectories("D:\\CokeeDP\\Cache"))
+                    {
+                        var dir = str.Replace("D:\\CokeeDP\\Cache\\", "");
+                        if (dir != "2024") dirs.Add(dir);
+                    }
+                    foreach (string str in Directory.GetDirectories("D:\\CokeeDP\\Cache\\2024"))
+                    {
+                        var dir = str.Replace("D:\\CokeeDP\\Cache\\2024\\", "");
+                        dirs.Add($"v2:{dir}");
+                    }
+                    dirlist.ItemsSource = dirs;
+                    break;
+
+                case "2":
+                    string? a = dirlist.SelectedValue?.ToString();
+                    if (!string.IsNullOrEmpty(a))
+                    {
+                        if (a.StartsWith("v2:"))
+                            Directory.Delete($"D:\\CokeeDP\\Cache\\2024\\{a.Split(":")[1]}", true);
+                        else Directory.Delete($"D:\\CokeeDP\\Cache\\{a}", true);
+                        Catalog.ShowInfo($"Deleted dir {a}.");
+                    }
+                    break;
+            }
+        }
+    }
+}
